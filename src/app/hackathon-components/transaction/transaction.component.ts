311241a5bbdfe466da04229d87055098d955af55
Ravindra Naik <naik899@users.noreply.github.com>
2021-06-27 14:41:01 +0530

    transactions card added
    transactions card added

--- src/app/hackathon-components/transaction/transaction.component.ts
+++ src/app/hackathon-components/transaction/transaction.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { transactions } from 'src/app/theme/shared/models/transactions.model';
+import { LoaderService } from 'src/app/theme/shared/services/loader.service';
 import { TransactionService } from 'src/app/theme/shared/services/transactions/transaction.service';
 
 @Component({
@@ -10,15 +11,19 @@ import { TransactionService } from 'src/app/theme/shared/services/transactions/t
 export class TransactionComponent implements OnInit {
 
   transactionList :  transactions[] = null;
-  constructor(private transactionService: TransactionService) { }
+  constructor(private transactionService: TransactionService, private loaderService: LoaderService) { }
 
   async ngOnInit() {
+    this.loaderService.showLoader();
     let transactions = await this.transactionService.getTransactions();
     if(transactions != null && transactions.data.items.length > 0)
     {
       this.transactionList = transactions.data.items;
       console.log(this.transactionList);
+
     }
+
+    this.loaderService.hideLoader();
    
   }
 
